5ab427ddae7c1f8e32659f244d36f5f21c9715c0
Miki12392 <mbudziszewsk94@gmail.com>
2026-03-28 12:29:58 +0100

    wersja z kodami (działającz telegram)

--- live/notifier.py
+++ live/notifier.py
@@ -7,6 +7,8 @@ def send_telegram_msg(msg: str):
         return
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     try:
-        requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": msg}, timeout=5)
-    except:
-        pass
+        response = requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": msg}, timeout=5)
+        if response.status_code != 200:
+            print(f"❌ [TELEGRAM ERROR] Kod: {response.status_code}, Treść: {response.text}")
+    except Exception as e:
+        print(f"❌ [CONNECTION ERROR] {e}")
